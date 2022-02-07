355ee27e02762e2a7b931f4df754b69d7869951d
Serge Pillay <urza45@live.fr>
2022-02-07 20:48:19 +0100

    Correted js and css

--- public/js/modal.js
+++ public/js/modal.js
@@ -4,23 +4,25 @@ $(".userinfo").click(function(){
     var newTitle = $(this).data("title");
     // AJAX request
     $.ajax({
-         url: urlDest,
-         type: "post",
-         data: {userId: userId},
-         success: function(response){ 
-               // Add response in Modal body
-               $(".modal-body").html(response);
+        url: urlDest,
+        type: "post",
+        data: {
+            userId: userId
+        },
+        success: function(response){ 
+            // Add response in Modal body
+            $(".modal-body").html(response);
             $(".modal-title").html(newTitle);   
             // Display Modal
             $("#empModal").modal("show");
-         },
+        },
         error: function(){
             $(".modal-body").html("Une erreur est survenue.");
             $(".modal-title").html("Erreur");
             // Display Modal
             $("#empModal").modal("show");
         }
-   });
+    });
 });
 
 $(".close").click(function(){
